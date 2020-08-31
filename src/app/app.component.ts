191818bd5dc754a4efe4d31a979e9f028af6604a
Luis Morales <lucho-1100@hotmail.com>
2020-08-31 11:31:43 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basics-assignment-three-start';
+  password = 'P1234';
+  showPassword = false;
+
+  displaysTimes = [];
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+
+    if (this.showPassword) {
+      this.displaysTimes.push(new Date());
+    }
+  }
+
+  getBackgroundColor(index) {
+    if (index >= 4) {
+      return 'blue';
+    }
+  }
 }
